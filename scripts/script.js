bd20798fdbb579efed4f1296d15bbe936de145fd
Vasiliy Lavrov <thewargas@mail.ru>
2022-12-09 17:47:07 +0300

    Feature: сделать попап подтверждения формы

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,36 @@
+const form = document.querySelector(`.feedback__form`);
+const popup = document.querySelector(`.popup`);
+
+
+function handleCloseByEsc(event) {
+    const key = event.key;
+    if (key === "Escape") {
+        closePopup();
+    }
+}
+function openPopup() {
+    popup.classList.add(`popup_active`);
+    document.addEventListener("keydown", handleCloseByEsc);
+}
+
+function closePopup() {
+    popup.classList.remove(`popup_active`);
+    document.removeEventListener("keydown", handleCloseByEsc);
+}
+form.addEventListener(`submit`, (evt) => {
+    evt.preventDefault();
+    form.reset();
+    openPopup();
+});
+popup.addEventListener(`click`, (event) => {
+    if (
+        event.target.classList.contains("popup_active") ||
+        event.target.classList.contains("popup__close-button-image")
+    ) {
+        closePopup();
+    }
+});
+
 document.addEventListener("DOMContentLoaded", function () {
     let layer = document.querySelector('.feedback__image');
     document.addEventListener('mousemove', (event) => {
